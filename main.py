718f3d2c1dee0e9d8ce317ab9c64dce337d12d22
Kevin Roberts <kev.roberts@gmail.com>
2024-02-01 22:11:26 +0000

    Changed driver to use Pico_ePaper from phoreglad. Landscape working but weird issues with partial refresh and sleeping

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from Pico_ePaper_37 import EPD_3in7 # ePaper driver
+from Pico_ePaper import EinkPIO # This is preferrable to using Eink
+import framebuf
+from writer import Writer
+import freesans20
+
 import webConnect # handles connecting to WiFi
 import apiRequest # Calls Flood Level API
 import apiRequestTrend # Calls Flood Level API trend
+
 import time
 import ntptime # Acquire current time
 
@@ -10,11 +15,28 @@ import gc # For garbage collection!
 #import os # For checking storage
 
 # Initialize the e-paper display
-epd = EPD_3in7()
-    
-#epd.image1Gray.fill(0xff)
-#epd.image4Gray.fill(0xff)
+epd = EinkPIO(rotation=90, use_partial_buffer=True)
+epd.fill() # Clear screen on first run
+epd.show()
+epd.partial_mode_on() # Enabling partial mode blocks the use of gray
 
+'''
+class DummyDevice(framebuf.FrameBuffer):
+    def __init__(self, width, height, buf_format):
+        self.width = width
+        self.height = height
+        self._buf = bytearray(self.width * self.height // 8)
+        super().__init__(self._buf, self.width, self.height, buf_format)
+        self.fill(1)
+
+# Create DummyDevice object with the same dimensions as the display.
+dummy = DummyDevice(epd.width, epd.height, framebuf.MONO_HLSB)
+# Create Writer instance using dummy as device and freesans20 font.
+wri = Writer(dummy, freesans20)
+# Setup Writer (refer to documentation for details).
+wri.set_clip(row_clip=True, col_clip=True, wrap=True)
+'''
+# Connect to internet
 try:
     webConnect.connect()
 except KeyboardInterrupt:
@@ -36,10 +58,7 @@ while True:
     #print(f"Free storage: {s[0]*s[3]/1024} KB")
     print(f"Memory: {gc.mem_alloc()} of {gc.mem_free()} bytes used.")
     gc.collect() # garbage collection
-    
-    # Clear display - otherwise new text will displayed on top
-    epd.image4Gray.fill(0xff)
-    
+    '''
     # Get API values
     apiResults = apiRequest.request() # A list of values from API
     currentLevel,latestReading,typicalRangeHigh,typicalRangeLow,state = apiResults # Unpack list in this order
@@ -51,29 +70,31 @@ while True:
     currentLevel = "Current Level: " + str(currentLevel)
     state = "State: " + state
     trend = "Trend: " + apiRequestTrend.requestTrend()
-    
+
     # Display the river level values
-    epd.image4Gray.text(latestReading, 60, 10, epd.black)
-    epd.image4Gray.text(currentLevel, 60, 40, epd.black)
-    epd.image4Gray.text(state, 100, 70, epd.black)
-    epd.image4Gray.text(trend, 80, 100, epd.black)
-    
+    epd.text(latestReading, 60, 10, epd.black)
+    epd.text(currentLevel, 60, 40, epd.black)
+    epd.text(state, 100, 70, epd.black)
+    epd.text(trend, 80, 100, epd.black)
+    '''
     # Display the current time
     t = time.localtime()
-    epd.image4Gray.text("{}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}".format(
+    epd.text("{}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}".format(
             t[0], t[1], t[2], t[3], t[4], t[5]
-            ), 60, 140, epd.darkgray)
+            ), 60, 140, c=epd.black)
 
     # Display vanity
-    epd.image4Gray.text("Written by Kevin Roberts", 50, 180, epd.grayish)
-    
-    # Buffer? This is required
-    epd.EPD_3IN7_4Gray_Display(epd.buffer_4Gray)
-    # Partial Update?
-    #epd.EPD_3IN7_1Gray_Display_Part(epd.buffer_1Gray)
+    epd.text("Written by Kevin Roberts", 50, 180, c=epd.lightgray)
 
+    epd.show()
+
+    # Clear display - otherwise new text will displayed on top
+    epd.fill() # Clear display
+    epd.rect(60, 140, 160, 10, epd.white, f=True) # x, y, wide, high
+    
     # Wait a minute
-    #epd.Sleep() # Put ePaper to sleep 
-    time.sleep(60) # API updates every 15 minutes 60*15 = 900
+    #epd.sleep() # Put ePaper to sleep
+    time.sleep(10) # API updates every 15 minutes 60*15 = 900
     print("refreshing now")
+    #epd.reinit()
     
